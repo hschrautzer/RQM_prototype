6f7d43493345b1d5eafd95a455f972230b74e886
Hendrik Schrautzer <h.schrautzer@gmail.com>
2026-03-16 14:52:31 +0100

    added retraction and parallel transport

--- prototype/lbfgs.py
+++ prototype/lbfgs.py
@@ -5,14 +5,17 @@ from utils import *
 import logging
 from pathlib import Path
 from typing import Union
+
 """
 Recreated from Spinaker subroutine lbfgs_step_noncurved
 and its calling subroutine
 Additional source: Ivanov, et al, 2021, Appendix F (https://doi.org/10.1016/j.cpc.2020.107749).
 """
+
+
 class lbfgs_minimizer:
 	# Hyperparameters
-	N_memory: int 				# Number of entries in memory
+	N_memory: int  # Number of entries in memory
 	theta_max: np.float64
 
 	# Persistent variables
@@ -21,13 +24,13 @@ class lbfgs_minimizer:
 	rq_step_prev: np.ndarray
 	steplength_prev: np.float64
 	X_prev: np.ndarray
-	rho: np.ndarray 				# (d·y)**-1
+	rho: np.ndarray  # (d·y)**-1
 	gamma: np.ndarray
-	d: np.ndarray 					# Difference in spin configurations in 2N
+	d: np.ndarray  # Difference in spin configurations in 2N
 	y: np.ndarray
 
 	# Volatile variables
-	force: np.ndarray # 2xN_spinsxN_modes
+	force: np.ndarray  # 2xN_spinsxN_modes
 	rq_step: np.ndarray
 	steplength: np.float64
 
@@ -45,30 +48,30 @@ class lbfgs_minimizer:
 				N_modes: int,
 				N_iter: int,
 				rq_grad_tol: float):
-			
-			self.N_memory = N_memory
-			self.theta_max = theta_max
-			self.N_spins = N_spins
-			self.N_modes = N_modes
-			self.N_iter = N_iter
-			self.rq_grad_tol = rq_grad_tol
-			del N_memory
-			del theta_max
-			del N_spins
-			del N_modes
-			del N_iter
-			del rq_grad_tol
-			# Initialization
-			self.rq_step = np.zeros([2*self.N_spins, self.N_modes])
-			self.rho = np.zeros([self.N_memory])
-			self.d = np.zeros([2*self.N_spins, self.N_modes, self.N_memory])
-			self.y = np.zeros([2*self.N_spins, self.N_modes, self.N_memory])
-			self.gamma = np.zeros([self.N_memory])
-			self.dummy_step = np.zeros([2*self.N_spins,self.N_modes])
-			self.step_previous_dummy = np.zeros([2*self.N_spins, self.N_modes])
 
-			self.result["warnings"] = []
-			self.result["rq_gradient_norm"] = []
+		self.N_memory = N_memory
+		self.theta_max = theta_max
+		self.N_spins = N_spins
+		self.N_modes = N_modes
+		self.N_iter = N_iter
+		self.rq_grad_tol = rq_grad_tol
+		del N_memory
+		del theta_max
+		del N_spins
+		del N_modes
+		del N_iter
+		del rq_grad_tol
+		# Initialization
+		self.rq_step = np.zeros([2 * self.N_spins, self.N_modes])
+		self.rho = np.zeros([self.N_memory])
+		self.d = np.zeros([2 * self.N_spins, self.N_modes, self.N_memory])
+		self.y = np.zeros([2 * self.N_spins, self.N_modes, self.N_memory])
+		self.gamma = np.zeros([self.N_memory])
+		self.dummy_step = np.zeros([2 * self.N_spins, self.N_modes])
+		self.step_previous_dummy = np.zeros([2 * self.N_spins, self.N_modes])
+
+		self.result["warnings"] = []
+		self.result["rq_gradient_norm"] = []
 
 	def calc_step(self, force):
 		# Selecting memory index for LIFO history
@@ -77,12 +80,12 @@ class lbfgs_minimizer:
 			# First iteration
 			self.rq_step = force
 		else:
-			self.d[:,:, ind_memory] = self.steplength_prev * self.rq_step_prev
-			self.y[:,:, ind_memory] = self.force_prev - force
+			self.d[:, :, ind_memory] = self.steplength_prev * self.rq_step_prev
+			self.y[:, :, ind_memory] = self.force_prev - force
 
-			y_dot_d = np.dot(self.y[:,:,ind_memory].ravel(),
-							 self.d[:,:,ind_memory].ravel()) # SHARP
-			self.rho[ind_memory] = 1/y_dot_d
+			y_dot_d = np.dot(self.y[:, :, ind_memory].ravel(),
+							 self.d[:, :, ind_memory].ravel())  # SHARP
+			self.rho[ind_memory] = 1 / y_dot_d
 
 			if self.rho[ind_memory] < 0:
 				self.rq_step = force
@@ -90,52 +93,53 @@ class lbfgs_minimizer:
 				return
 			q = -1.0 * force
 
-			for l in range(self.N_memory, 0, -1): # MEGASHARP (+-)1?
-				j = np.mod(l+ind_memory,self.N_memory) # (+-)1?, also, odd that this is adding memory index as an offset
-				q_dot_d = np.dot(q[:,:,:].ravel(),
-							 self.d[:,:,j].ravel()) #SHARP?
-				self.gamma[j] = self.rho[j]*q_dot_d
-				q[:] = q[:] - self.gamma[j] * self.y[:,:,j]
+			for l in range(self.N_memory, 0, -1):  # MEGASHARP (+-)1?
+				j = np.mod(l + ind_memory,
+						   self.N_memory)  # (+-)1?, also, odd that this is adding memory index as an offset
+				q_dot_d = np.dot(q[:, :, :].ravel(),
+								 self.d[:, :, j].ravel())  # SHARP?
+				self.gamma[j] = self.rho[j] * q_dot_d
+				q[:] = q[:] - self.gamma[j] * self.y[:, :, j]
 
-			y_dot_y = np.dot(self.y[:,:, ind_memory].ravel(),
-							 self.d[:,:,ind_memory].ravel()) #SHARP
-			
-			self.dummy_step[:,:] = q[:,:,:] / (self.rho[ind_memory]* y_dot_y)
-			
-			for l in range(1,self.N_memory): # (+-)1?
+			y_dot_y = np.dot(self.y[:, :, ind_memory].ravel(),
+							 self.d[:, :, ind_memory].ravel())  # SHARP
+
+			self.dummy_step[:, :] = q[:, :, :] / (self.rho[ind_memory] * y_dot_y)
+
+			for l in range(1, self.N_memory):  # (+-)1?
 				if self.iteration <= self.N_memory:
 					j = l
 				else:
-					j = np.mod(l+ind_memory,self.N_memory) # (+-)1?
-				res = np.dot(self.y[:,:,j].ravel(),
-							 self.dummy_step[:,:].ravel()) #SHARP
-				self.dummy_step[:,:] = self.dummy_step[:,:] \
-								+ self.d[:,:,j] * (self.gamma[j]-self.rho[j]*res)
-			self.rq_step = -1.0*self.dummy_step
+					j = np.mod(l + ind_memory, self.N_memory)  # (+-)1?
+				res = np.dot(self.y[:, :, j].ravel(),
+							 self.dummy_step[:, :].ravel())  # SHARP
+				self.dummy_step[:, :] = self.dummy_step[:, :] \
+										+ self.d[:, :, j] * (self.gamma[j] - self.rho[j] * res)
+			self.rq_step = -1.0 * self.dummy_step
 
-	def step(self,force):
+	def step(self, force):
 		self.calc_step(force)
-		theta_rms = np.dot(self.rq_step[:,:].ravel(),
-							self.rq_step[:,:].ravel()) #SHARP
+		theta_rms = np.dot(self.rq_step[:, :].ravel(),
+						   self.rq_step[:, :].ravel())  # SHARP
 		theta_rms = np.sqrt(theta_rms)
-		t_res = self.theta_max/theta_rms
+		t_res = self.theta_max / theta_rms
 		if t_res < 1:
 			self.steplength = t_res
 		else:
 			self.steplength = np.float64(1.0)
 
 	def rq_force_calc(self, mag: Magnetization, X_n2: np.ndarray) -> tuple[np.ndarray, float]:
-		t_HX = mag.finite_difference_HX(X_n2) # 2N_spins x N_modes
+		t_HX = mag.finite_difference_HX(X_n2)  # 2N_spins x N_modes
 
 		rq_matrix = np.zeros([self.N_modes, self.N_modes])
-		rq_matrix = np.reshape(X_n2, [2,-1]).T @ t_HX #SHARP
+		rq_matrix = np.reshape(X_n2, [2, -1]).T @ t_HX  # SHARP
 
 		rq = np.linalg.trace(rq_matrix)
 
 		# Check this ↓↓↓
-		rq_gradient = t_HX # 2N_spins x N_modes
-		rq_gradient = rq_gradient - X_n2 @ rq_matrix # 2N_spins x N_modes - 2N_spins x N_modes @ N_modes x N_modes
-		rq_gradient = 2 * rq_gradient # 2N_spins x N_modes
+		rq_gradient = t_HX  # 2N_spins x N_modes
+		rq_gradient = rq_gradient - X_n2 @ rq_matrix  # 2N_spins x N_modes - 2N_spins x N_modes @ N_modes x N_modes
+		rq_gradient = 2 * rq_gradient  # 2N_spins x N_modes
 
 		rq_gradient_norm = float(np.linalg.norm(rq_gradient, ord='fro'))
 		self.result['rq_gradient_norm'].append(rq_gradient_norm)
@@ -144,14 +148,14 @@ class lbfgs_minimizer:
 		return rq_force, rq_gradient_norm
 
 	def minimize(self, mag: Magnetization, vec_ini: np.ndarray):
-		v_fin_n3 = np.zeros([3*self.N_spins])
+		v_fin_n3 = np.zeros([3 * self.N_spins])
 		rq = 0
-		X_n2 = np.zeros([2*self.N_spins, self.N_modes])
+		X_n2 = np.zeros([2 * self.N_spins, self.N_modes])
 		evals = 0
 		for ind_modes in range(self.N_modes):
-			vec_ini[:,ind_modes] = mag.project_to_basis(vec_ini[:,ind_modes])
-			vec_ini[:,ind_modes] = vec_ini[:,ind_modes] / norm_n2
-		
+			vec_ini[:, ind_modes] = mag.project_to_basis(vec_ini[:, ind_modes])
+			vec_ini[:, ind_modes] = vec_ini[:, ind_modes] / norm_n2
+
 		X_n2 = GM_retraction(X_n2)
 
 		# t_HX = mag.finite_difference_HX(X_n2) # 2N_spins x N_modes
@@ -178,21 +182,30 @@ class lbfgs_minimizer:
 				self.result['warnings'].append("RQM exceeded iterations")
 				self.result['status'] = "not converged"
 			self.step(rq_force)
-			self.force_prev = self.force
+
+			# @Olafur (I commented this out, this is supposed to be updated by parallel transport)
+			#self.force_prev = self.force
 			self.steplength_prev = self.steplength
-			self.rq_step_prev = self.rq_step
+			# @Olafur (I commented this out, this is supposed to be updated by parallel transport)
+			#self.rq_step_prev = self.rq_step
 
 			self.X_prev = X_n2
 
 			U, S, VT = np.linalg.svd(self.rq_step)
 
-			X_n2 = GM_retraction_exp(X=X_n2,U=U,S=S,VT=VT,delta=self.steplength)
-			# grassman retraction w. USV
-			# transports w. USV
+			# Retract the configuration
+			X_n2 = GM_retraction_exp(X=X_n2, U=U, S=S, VT=VT, delta=float(self.steplength))
+			# For efficiency the parallel transport is done in 2 steps: computing the transport matrix and applying the
+			# transport matrix
+			TM = GM_calc_transportmatrix(X=self.X_prev,U=U,S=S,VT=VT,delta=float(self.steplength_prev))
+			self.rq_step_prev = GM_parallel_transport(b=self.rq_step,U=U,TM=TM)
+			self.force_prev = GM_parallel_transport(b=self.force,U=U,TM=TM)
+			for k in range(self.N_memory):
+				self.d[:,:,k] = GM_parallel_transport(self.d[:,:,k],U=U,TM=TM)
+				self.y[:,:,k] = GM_parallel_transport(self.y[:,:,k],U=U,TM=TM)
 
-
-
-
-
-
+			# Orthogonalize (to avoid accumulating numerical errors)
+			X_n2 = GM_retraction(X_n2)
+			# Quantities for the next iteration
+			rq_force, rq_gradient_norm = self.rq_force_calc(mag, X_n2)
 		return self.result
